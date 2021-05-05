23fdc6a694a21906c8936e581df9422e641bf2ba
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-05 13:38:19 +1200

    add react component files

---
+++ client/components/admin/NavBarAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBarAdmin () {
+  return (
+    <nav>
+      <li>Home</li>
+      <li>Log Out</li>
+    </nav>
+  )
+}
---
+++ client/components/customer/NavBarCustomer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NavBarCustomer () {
+  return (
+    <nav>
+      <li>Home</li>
+      <li>Log out</li>
+    </nav>
+  )
+}
