bdfb22f6a090922fbb9ac8364132d92294065778
cherrycrush <phobosnz@gmail.com>
2021-05-06 09:55:12 +1200

    Import signup component into App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import LogIn from './LogIn'
 import AdminHome from './admin/AdminHome'
-import CustomerHome from './customer/CustomerHome'
+import CustomerHome from './customer/CustomerHome' 
+import SignUp from './SignUp'
 
 const App = () => {
   return (
     <>
       <LogIn />
-      <AdminHome />
-      <CustomerHome />
+      <SignUp />
+      {/* <AdminHome />
+      <CustomerHome /> */}
     </>
   )
 }
