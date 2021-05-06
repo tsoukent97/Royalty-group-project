de31433524c6f9d179e9f3540899600b1461090b
nathan-mclauchlan <nathanmclauchlan21@gmail.com>
2021-05-06 15:27:48 +1200

    updated customers info

--- server/db/migrations/20210505023110_customers.js
+++ server/db/migrations/20210505023110_customers.js
@@ -2,6 +2,7 @@ exports.up = (knex) => {
   return knex.schema.createTable('customers', (table) => {
     table.increments('id').primary()
     table.string('name')
+    table.string('user_name')
     table.string('user_type')
     table.binary('hash')
   })
