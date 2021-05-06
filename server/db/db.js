8817cf109746b0877a6f8c283272ae4b2946f29b
sam-taylor165 <taylsj1908@gmail.com>
2021-05-06 15:28:35 +1200

    start passport

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,20 @@
 const config = require('./knexfile').development
 const connection = require('knex')(config)
 
-module.exports = {
-  getCustomers
-}
-
 function getCustomers (db = connection) {
   return db('customers').select()
+}
+
+const customerExists = (username, db = connection) => {
+  return db('customers')
+    .count('id as n')
+    .where('username', username)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
+
+module.exports = {
+  getCustomers,
+  customerExists
 }
