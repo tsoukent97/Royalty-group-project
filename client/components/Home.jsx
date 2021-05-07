eeaf262f2c79ef2894ef649af31c8392ced8e041
Kent Tsou <tsoukent97@gmail.com>
2021-05-07 15:28:18 +1200

    home.jsx

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Home () {
+
   return (
+    <>
     <div>
       <h2>Home Page</h2>
+      <button>Signup</button>
+      <button>Login</button>
     </div>
+    </>
   )
 }
 
