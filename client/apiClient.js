38b0f48f7e6bf377a8689748c590357660c001a9
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-07 15:45:34 +1200

    deleted duplicate api and db functions

--- client/apiClient.js
+++ client/apiClient.js
@@ -2,12 +2,6 @@ import request from 'superagent'
 
 const customerServer = 'http://localhost:3000/customer'
 const businessServer = 'http://localhost:3000/business'
-const landingServer = 'http://localhost:3000'
-
-export function getCustomerProfile (id) {
-  return request.get(customerServer + '/' + id)
-    .then(res => res.body)
-}
 
 export function getCustomerCards (id) {
   return request.get(customerServer + '/' + id + '/cards')
@@ -33,20 +27,6 @@ export function getCustomers (id) {
 export function deleteBusiness (id) {
   return request
     .patch(businessServer + '/' + id + '/delete')
-    .then(res => res.body)
-}
-
-export function addCustomer (name, userName) {
-  return request
-    .post(landingServer)
-    .send(name, userName)
-    .then(res => res.body)
-}
-
-export function addBusiness (name, address, phoneNumber, email) {
-  return request
-    .post(landingServer)
-    .send(name, address, phoneNumber, email)
     .then(res => res.body)
 }
 
