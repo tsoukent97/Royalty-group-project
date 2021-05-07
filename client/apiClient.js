810d8e9808e356b35b8c0d8ac744ec28171a5bed
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-07 14:55:22 +1200

    add api functions

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,1 +1,52 @@
 import request from 'superagent'
+
+const customerServer = 'http://localhost:3000/customer'
+const businessServer = 'http://localhost:3000/business'
+const landingServer = 'http://localhost:3000'
+
+export function getCustomerProfile (id) {
+  return request.get(customerServer + '/' + id)
+    .then(res => res.body)
+}
+
+export function getCustomerCards (id) {
+  return request.get(customerServer + '/' + id + '/cards')
+    .then(res => res.body)
+}
+
+export function deleteCustomer (id) {
+  return request
+    .patch(customerServer + '/' + id + '/delete')
+    .then(res => res.body)
+}
+
+export function getBusinessProfile (id) {
+  return request.get(businessServer + '/' + id)
+    .then(res => res.body)
+}
+
+export function getCustomers (id) {
+  return request.get(businessServer + '/' + id + '/customers')
+    .then(res => res.body)
+}
+
+export function deleteBusiness (id) {
+  return request
+    .patch(businessServer + '/' + id + '/delete')
+    .then(res => res.body)
+}
+
+export function addCustomer (name, userName) {
+  return request
+    .post(landingServer)
+    .send(name, userName)
+    .then(res => res.body)
+}
+
+export function addBusiness (name, address, phoneNumber, email) {
+  return request
+    .post(landingServer)
+    .send(name, address, phoneNumber, email)
+    .then(res => res.body)
+}
+
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Login from './Login'
-import AdminHome from './admin/AdminHome'
+import BusinessHome from './business/BusinessHome'
 import CustomerHome from './customer/CustomerHome'
 import SignUp from './SignUp'
 import { HashRouter as Router, Route } from 'react-router-dom'
@@ -9,9 +9,10 @@ const App = () => {
   return (
     <>
       <Router>
-        <Route exact path={"/login"} component={Login} />
-        <Router exact path={"/Customerhome"} component={CustomerHome} />
-        <Router exact path={"/Businesshome"} component={BusinessHome} />
+        <Route exact path={'/SignUp'} component={SignUp} />
+        <Route exact path={'/Login'} component={Login} />
+        <Router exact path={'/Customerhome'} component={CustomerHome} />
+        <Router exact path={'/Businesshome'} component={BusinessHome} />
       </Router>
     </>
   )
