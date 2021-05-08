9073d2d92ab27b10309c5723b5d7e4907b87bb9a
cherrycrush <phobosnz@gmail.com>
2021-05-09 00:12:28 +1200

    Add business nav

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,12 +5,14 @@ import CustomerHome from './customer/CustomerHome'
 import NavCustomer from './customer/NavCustomer'
 import SignUp from './SignUp'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import NavBusiness from './business/NavBusiness'
 
 const App = () => {
   return (
     <>
       <Router>
         <NavCustomer />
+        <NavBusiness />
         <SignUp />
         <Route exact path={'/login'} component={Login} />
         <Route exact path={'/CustomerHome'} component={CustomerHome} />
