8000dab6ec855d1479ce9c1ad79a7c772ca34234
cherrycrush <phobosnz@gmail.com>
2021-05-08 23:49:16 +1200

    Add semantic menu

--- client/components/customer/NavCustomer.jsx
+++ client/components/customer/NavCustomer.jsx
@@ -1,41 +1,17 @@
 import React, { Component } from 'react'
-import { Button, Dropdown, Menu } from 'semantic-ui-react'
+import { Container, Menu } from 'semantic-ui-react'
 
-export default class NavCustomer extends Component() {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render () {
-    const { activeItem } = this.state
-
-    return (
-      <Menu size='large'>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
-
-        <Menu.Menu position='right'>
-          <Dropdown item text='Language'>
-            <Dropdown.Menu>
-              <Dropdown.Item>English</Dropdown.Item>
-              <Dropdown.Item>Russian</Dropdown.Item>
-              <Dropdown.Item>Spanish</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <Menu.Item>
-            <Button primary>Sign Up</Button>
-          </Menu.Item>
-        </Menu.Menu>
+export default function NavCustomer () {
+  return (
+    <Container>
+      <Menu stackable>
+        <Menu.Item>
+          <img src='/logo.png' />
+        </Menu.Item>
+        <Menu.Item>Features</Menu.Item>
+        <Menu.Item>Testimonials</Menu.Item>
+        <Menu.Item>Sign-in</Menu.Item>
       </Menu>
-    )
-  }
+    </Container>
+  )
 }
