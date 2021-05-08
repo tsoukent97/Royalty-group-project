b308b392d99749fc5dd19b797416eda98e75be57
cherrycrush <phobosnz@gmail.com>
2021-05-09 00:02:33 +1200

    Modify menu item size, pos

--- client/components/customer/NavCustomer.jsx
+++ client/components/customer/NavCustomer.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
-import { Container, Menu } from 'semantic-ui-react'
+import React from 'react'
+import { Container, Button, Menu } from 'semantic-ui-react'
 
 export default function NavCustomer () {
   return (
     <Container>
-      <Menu stackable>
+      <Menu stackable widths={5}>
         <Menu.Item>
-          <img src='/logo.png' />
+          <img src='/logo.png' alt='Our logo here' />
         </Menu.Item>
-        <Menu.Item>Features</Menu.Item>
-        <Menu.Item>Testimonials</Menu.Item>
-        <Menu.Item>Sign-in</Menu.Item>
+        <Menu.Item>Welcome, username here</Menu.Item>
+        <Menu.Item>Your Royalty Cards</Menu.Item>
+        <Menu.Item position='right'>
+          <Button primary>Add a card</Button>
+        </Menu.Item>
+        <Menu.Item position='right'>
+          <Button secondary>Logout</Button>
+        </Menu.Item>
       </Menu>
     </Container>
   )
