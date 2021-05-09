d092f83635d7106c5f15ec45dba718f6cf1e6943
cherrycrush <phobosnz@gmail.com>
2021-05-10 09:18:28 +1200

    Prepare for merge

--- client/components/customer/CustomerHome.jsx
+++ client/components/customer/CustomerHome.jsx
@@ -4,52 +4,56 @@
 
 import React from 'react'
 // import NavCustomer from './NavCustomer'
-import { Grid, Image } from 'semantic-ui-react'
+import { Container, Grid, Image } from 'semantic-ui-react'
 import NavCustomer from './NavCustomer'
 
 function CustomerHome () {
   return (
-    <div>
-      <NavCustomer />
-      <Grid className='card-grid' relaxed columns={3}>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image2' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-        <Grid.Column>
-          <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
-        </Grid.Column>
-      </Grid>
-    </div>
+    <>
+      <div>
+        <NavCustomer />
+      </div>
+      <Container textAlign='justified'>
+        <Grid className='card-grid' relaxed columns={3}>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image2' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+          <Grid.Column>
+            <Image className='card' href='#' src='/images/placeholder.png' alt='Image1' />
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </>
   )
 }
 
