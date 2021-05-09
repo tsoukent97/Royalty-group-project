916c449972c1919b7726c601f2b24df0cc44bdb3
cherrycrush <phobosnz@gmail.com>
2021-05-09 20:19:03 +1200

    Add nav component to render in customer home

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -3,78 +3,73 @@ import React, { useState } from 'react'
 let userInfo = {}
 
 function Login (props) {
-    const initialData = {
-        username: '',
-        password: '',
-    }
+  const initialData = {
+    username: '',
+    password: ''
+  }
 
-    const [form, setForm] = useState(initialData)
+  const [form, setForm] = useState(initialData)
 
-    function handleChange (e) {
-        const { name, value } = e.target
-        setForm({
-            ...form,
-            [name]:value
-        })
-    }
+  function handleChange (e) {
+    const { name, value } = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
 
-    function handleSubmit (e) {
-        e.preventDefault()
-        userInfo = form
-        props.history.push(props.isCustomer ? '/Customerhome': '/Businesshome')
-        return null
-    }
-    function toggleBusiness (e) {
-        e.preventDefault()
-        props.history.push(props.isCustomer ? '/BusinessLogin': '/CustomerLogin')
-    }
+  function handleSubmit (e) {
+    e.preventDefault()
+    userInfo = form
+    props.history.push(props.isCustomer ? '/Customerhome': '/Businesshome')
+    return null
+  }
+  function toggleBusiness (e) {
+    e.preventDefault()
+    props.history.push(props.isCustomer ? '/BusinessLogin': '/CustomerLogin')
+  }
 
-    function homePath (e) {
-        e.preventDefault()
-        props.history.push('/')
-    }
+  function homePath (e) {
+    e.preventDefault()
+    props.history.push('/')
+  }
 
-    return (
-        <>
-        <label></label>
-        <button onClick={toggleBusiness}>{props.isCustomer? 'Business Login': 'Customer Login'}</button>
-        <div>
-           <form>
-           <h2>{props.isCustomer ? 'Customer': 'Business'} login page</h2>
-                <label>{props.isCustomer ? 'Username:': 'Business:'}</label>
-                <input 
-                placeholder={props.isCustomer ? 'Enter username...': 'Enter business...'}
-                name='username' 
-                onChange={handleChange} 
-                value={form.username}
-                type='text'
-                required>
+  return (
+    <>
+      <label></label>
+      <button onClick={toggleBusiness}>{props.isCustomer? 'Business Login': 'Customer Login'}</button>
+      <div>
+        <form>
+          <h2>{props.isCustomer ? 'Customer': 'Business'} login page</h2>
+          <label>{props.isCustomer ? 'Username:': 'Business:'}</label>
+          <input 
+            placeholder={props.isCustomer ? 'Enter username...': 'Enter business...'}
+            name='username' 
+            onChange={handleChange} 
+            value={form.username}
+            type='text'
+            required
+          />
+          <br></br>
 
-                </input>
-
-                <br></br>
-
-                <label>Password:</label>
-                <input 
-                placeholder='Enter password...'
-                name='password' 
-                type='text'
-                onChange={handleChange} 
-                value={form.password}
-                required>
-                </input>
-                <br></br>
-                <button type='button' onClick={handleSubmit}>Login</button>
-                <br></br>
-                <br></br>
-                <br></br>
-        <div>
+          <label>Password:</label>
+          <input 
+            placeholder='Enter password...'
+            name='password' 
+            type='text'
+            onChange={handleChange} 
+            value={form.password}
+            required>
+          </input>
+          <br></br>
+          <button type='button' onClick={handleSubmit}>Login</button>
+          <div>
             <button onClick={homePath}>Home</button>
-        </div>
-            </form>
-        </div>
-        </>
-    )
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default Login
--- client/components/business/NavBusiness.jsx
+++ client/components/business/NavBusiness.jsx
@@ -3,7 +3,6 @@ import { Container, Button, Menu } from 'semantic-ui-react'
 
 export default function NavBusiness () {
   return (
-<<<<<<< HEAD
     <Container>
       <Menu stackable widths={5}>
         <Menu.Item>
@@ -16,12 +15,5 @@ export default function NavBusiness () {
         </Menu.Item>
       </Menu>
     </Container>
-=======
-    <nav>
-      <li>Home</li>
-      <li>Log Out</li>
-      <li><a href='/#/Businesshome/AccountInfo'>Account Information</a></li>
-    </nav>
->>>>>>> main
   )
 }
