ae96afe1242bd4cd7432d49bcf19116decdfac6b
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-11 13:06:20 +1200

    edit

--- client/components/customer/CustomerHome.jsx
+++ client/components/customer/CustomerHome.jsx
@@ -6,8 +6,10 @@ import React, { useState, useEffect } from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 import { getCustomerCards } from '../../api/apiClient'
 import NavCustomer from './NavCustomer'
+import { Link } from 'react-router-dom'
 
 const id = 902
+let businessId = 
 
 function CustomerHome () {
   const [state, setState] = useState([{
@@ -28,11 +30,17 @@ function CustomerHome () {
       })
   }, [])
 
+  function handleClick (id) {
+    businessId = id
+    // props.history.push('/Customerhome/cardInfo')
+    return null
+  }
+
   return (
     <div>
       <NavCustomer />
       <Grid relaxed columns={3}>
-        {state.map((card) => <Grid.Column key={card.id}><Image href='#' src={card.logo} alt={card.business} /></Grid.Column>)}
+        {state.map((card) => <Grid.Column key={card.id}><Link to={'/Customerhome/cardInfo'}><Image href='#' src={card.logo} alt={card.business} /></Link></Grid.Column>)}
       </Grid>
     </div>
   )
