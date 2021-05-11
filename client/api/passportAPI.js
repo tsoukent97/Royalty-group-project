a0674be32e27fade407953dbcda81db7b5bdfae3
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-12 10:35:59 +1200

    change localhost

--- client/api/passportAPI.js
+++ client/api/passportAPI.js
@@ -1,9 +1,7 @@
 import request from 'superagent'
 
-const rootUrl = 'http://localhost:3000'
-
 export function loginCustomer (customer) {
-  return request.post(rootUrl + '/login')
+  return request.post('/login')
     .send({
       username: customer.username,
       password: customer.password
@@ -12,7 +10,7 @@ export function loginCustomer (customer) {
 }
 
 export function registerUser (user) {
-  return request.post(rootUrl + '/register')
+  return request.post('/register')
     .send(user)
     .then(res => res.body)
 }
@@ -20,6 +18,6 @@ export function registerUser (user) {
 export function logOut () {
   console.log('api logout')
   return request
-    .post(rootUrl + '/logout')
+    .post('/logout')
     .then(res => res.body)
 }
