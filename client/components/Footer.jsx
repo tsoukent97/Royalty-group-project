cf88475f7b62107769d325953639239b84da2be0
cherrycrush <phobosnz@gmail.com>
2021-05-12 10:40:36 +1200

    Add semantic to card page

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Icon } from 'semantic-ui-react'
-// import { Container } from 'semantic-ui-react'
 
 function Footer () {
   return (
-    <div className='footer'>
-      <div className="ui segment">
-        <h4>Contact us</h4>
-        <div className="ui divider"></div>
-        <Icon name='envelope' inverted />
-        <p>loyaltea@post.com</p>
-        <h4 className="ui horizontal divider">
-        </h4>
+    <>
+      <br></br>
+      <div className='footer'>
+        <div className="ui segment">
+          <h4>Contact us</h4>
+          <div className="ui divider"></div>
+          <p><Icon name='envelope' />loyaltea@post.com</p>
+          <h4 className="ui horizontal divider">
+          </h4>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
