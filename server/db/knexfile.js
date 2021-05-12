e0a8b17808dd45acaa7e38b5857f68bb23457944
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-05-12 14:31:14 +1200

    heroku changes

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -41,8 +41,10 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
-    ssl: { rejectUnauthorized: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     pool: {
       min: 2,
       max: 10
